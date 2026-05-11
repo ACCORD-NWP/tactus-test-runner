7fcf5ed65301706888fc34cbe191485195c9741b
Martin Frølund <maf@dmi.dk>
2026-05-11 08:47:45 +0000

    Small correction

--- ttr/src/ttr.py
+++ ttr/src/ttr.py
@@ -245,7 +245,7 @@ class TestCases:
             # Save the modifications
             outfile = f"{self.test_dir}/modifs_{case}.toml"
             logger.info(" create: {}", outfile)
-            BasicConfig.save_dictionary_as(config["modifs"], outfile)
+            BasicConfig.save_as(config["modifs"], outfile)
 
             # Build the command to execute
             cmd = [
